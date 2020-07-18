c84f949e79de1b4710eee6eba6e4dedd999a59e8
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2020-07-18 18:18:49 +0100

    mvp done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,22 @@
+# def test_ex1_3(self):
+#     weights_3 = [4, 6, 10, 15, 16]
+#     answer_3 = get_indices_of_item_weights(weights_3, 5, 21)
+#     self.assertTrue(answer_3[0] == 3)
+#     self.assertTrue(answer_3[1] == 1)
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
     # Your code here
+    storage = {}
+
+    for i in range(length):
+        storage[weights[i]] = i
+
+    for i in range(length):
+        result = limit - weights[i]
+        if result in storage:
+            return (max(i, storage[result]), min(i, storage[result]))
 
     return None
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,27 @@
+# def test_small(self):
+#     result = has_negatives([1,2,3])
+#     self.assertTrue(result == [])
+
+#     result = has_negatives([1,2,3,-4])
+#     self.assertTrue(result == [])
+
+#     result = has_negatives([-1,-2,1,2,3,4,-4])
+#     result.sort()
+#     self.assertTrue(result == [1,2,4])]
+
+
 def has_negatives(a):
     """
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    storage = {}
 
+    for num in a:
+        storage[num] = 1
+        if num != 0 and -num in storage:
+            result.append(abs(num))
     return result
 
 
